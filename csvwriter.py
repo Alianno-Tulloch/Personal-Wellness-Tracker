fb2e9f508bf6d72633ecfec589ac2dc9fc8bef22
Alianno Tulloch <aliannotul@gmail.com>
2025-12-16 14:55:06 -0500

    initial commit, brainstorming

---
+++ csvwriter.py
@@ -0,0 +1,6 @@
+import csv
+
+    """
+    Writes data to a CSV file. Determines the format
+    """
+
---
+++ data_structuring.py
@@ -0,0 +1,14 @@
+
+
+
+# Test Entry
+daily_entry = {
+    "Date": "2024-06-15",
+    "Hours_Slept": 7.5,
+    "Exercise_Minutes": 45,
+    "Mood Scale": 8,
+    "Mood Description": "happy",
+    "Activities": ["jogging", "reading", "cooking"],
+    "Notes": "Had a great day exploring new recipes and enjoying the sunshine. "
+    "Felt energetic and positive throughout the day."
+}
